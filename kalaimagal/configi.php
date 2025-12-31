8e7256f21cf4ecdebd899384a2aeab7a51048172
Dinesh Ravichandran <deenaravichandran@gmail.com>
2025-12-31 10:24:55 +0530

    Initial project setup

--- kalaimagal/configi.php
+++ kalaimagal/configi.php
@@ -2,9 +2,9 @@
 session_start();
 date_default_timezone_set('Asia/Kolkata');
 
-define("DB_HOST", '13.126.52.143');
-define("DB_USER", 'main');
-define("DB_PASS", 'P@mani4u');
+define("DB_HOST", 'localhost');
+define("DB_USER", 'root');
+define("DB_PASS", '');
 define("DB_NAME", 'kalaimagal_thiru');
 
 $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
